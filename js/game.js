70b56242b9d82a1533b8d58713f9f07a112c25c7
Trofimov Alexander <trofas03@yandex.ru>
2022-08-21 18:21:37 +0300

    Field and Food Update

--- js/game.js
+++ js/game.js
@@ -2,14 +2,30 @@ const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
 const ground = new Image();
-ground.src = '.image/ground.png';
+ground.src = 'image/ground.png';
 
-const food = new Image();
-food.src = '.image/food.png';
+const foodImg = new Image();
+foodImg.src = 'image/food.png';
 
-let box = 48;
+let box = 32;
 
 let score = 0;
 
-function 
+let food = {
+    x: Math.floor((Math.random() * 17 + 1)) * box,
+    y: Math.floor((Math.random() * 15 + 1)) * box,
+};
 
+let snake = [];
+snake [0] = {
+    x: 9 * box,
+    y: 10 * box
+};
+
+function drawGame() {
+    ctx.drawImage(ground, 0, 0);
+
+    ctx.drawImage(foodImg, food.x, food.y);
+}
+
+let game = setInterval(drawGame, 100);
