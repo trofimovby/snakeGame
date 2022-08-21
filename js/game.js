fe78d2ec88e9a9373a4ac36b6c0b61deb90beca6
Trofimov Alexander <trofas03@yandex.ru>
2022-08-21 18:25:08 +0300

    A snake was added

--- js/game.js
+++ js/game.js
@@ -26,6 +26,11 @@ function drawGame() {
     ctx.drawImage(ground, 0, 0);
 
     ctx.drawImage(foodImg, food.x, food.y);
+
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
 }
 
 let game = setInterval(drawGame, 100);
