f0bb10507d6489f26f4f968439e211fea68fd611
Trofimov Alexander <trofas03@yandex.ru>
2022-08-21 18:05:55 +0300

    The playing field
    Added a picture of the playing field and food

---
+++ js/game.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+const ground = new Image();
+ground.src = '.image/ground.png';
+
+const food = new Image();
+food.src = '.image/food.png';
+
+let box = 48;
+
+let score = 0;
+
+function 
+
